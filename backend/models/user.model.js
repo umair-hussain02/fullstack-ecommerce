e65e647c81f6df4354378ca2dcd4aa9d00e3aa62
umair-hussain02 <bc230203492uhu@vu.edu.pk>
2025-05-11 15:39:07 +0500

    add user model

---
+++ backend/models/user.model.js
@@ -0,0 +1,113 @@
+import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
+const userSchema = new Schema(
+    {
+        firstName: {
+            type: String,
+            required: true,
+        },
+        lastname: {
+            type: String,
+            required: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        mobNumb: {
+            type: String,
+            required: true,
+            unique: true,
+        },
+        password: {
+            type: String,
+            required: true,
+        },
+        role: {
+            type: String,
+            default: "user",
+        },
+        isBlocked: {
+            type: Boolean,
+            default: false,
+        },
+        cart: {
+            type: Array,
+            default: [],
+        },
+        address: {
+            type: String,
+        },
+        wishlist: {
+            type: Schema.type.ObjectId,
+            ref: "Product",
+        },
+        passwordChangedAt: Date,
+        passwordResetToken: String,
+        passwordResetExpires: Date,
+    },
+    {
+        timestamps: true,
+    }
+);
+
+// Password Incryption...
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next();
+    this.password = await bcrypt.hash(this.password, 10);
+    next();
+});
+
+// Password Checking Method...
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password);
+};
+
+// Generate Access / Refresh Token
+userSchema.methods.generateAccessToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            firstName: this.firstName,
+            email: this.email,
+        },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY,
+        }
+    );
+};
+
+userSchema.methods.generateRefreshToken = async function () {
+    return jwt.sign(
+        {
+            _id: this._id,
+            firstName: this.firstName,
+            email: this.email,
+        },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY,
+        }
+    );
+};
+
+userSchema.methods.createPasswordResetToken = async function () {
+    // Generate a JWT (expires in 30 mins)
+    const resetToken = jwt.sign(
+        { id: this._id },
+        process.env.JWT_RESET_SECRET,
+        { expiresIn: "30m" }
+    );
+
+    // Optionally store a flag in DB (if needed)
+    this.passwordResetExpires = Date.now() + 30 * 60 * 1000; // 30 mins
+    await this.save();
+
+    return resetToken;
+};
+
+export const User = mongoose.model("User", userSchema);
